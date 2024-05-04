3a0f031c5481475a7ca7f7b043cc3a1b9e6ce334
MsSuraj001 <milukumar225@gmail.com>
2024-05-04 18:53:47 +0530

    This is my first language in Software Devlopment

---
+++ First.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  
+    int main (){
+   float redius;
+   printf("enter redius:");
+   scanf("%f", &redius);
+   float pi = 3.1415;
+   float area = pi * redius * redius;
+   printf("the are of cricle is ; %f.area");
+
+
+     return 0;
+     }
---
+++ hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    // printf("hello world\n");
+    // printf("my name is suraj");
+    int age;
+    printf("enter age;");
+    scanf("%d",&age);
+    if(age>18){
+        printf("adulte\n");
+    }
+    else{
+        printf("not adult \n");
+    }
+    return 0;   
+}
+
+
